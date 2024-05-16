8c9d8a38ca07e7f632fe3300d24e5fbc33b0ef37
GiraKoo <girakoo@163.com>
2024-05-16 14:14:00 +0800

    feat: 允许交互式输入路径，使用本工具

--- dump-helper.py
+++ dump-helper.py
@@ -5,15 +5,41 @@ import os
 import sys
 import subprocess
 
+
+def colorful_print(level, msg):
+    if level == 'error':
+        print(f"\033[31m{msg}\033[0m")
+    elif level == 'warning':
+        print(f"\033[33m{msg}\033[0m")
+    elif level == 'info':
+        print(f"\033[32m{msg}\033[0m")
+    else:
+        print(msg)
+
+colorful_print('info', "********************************************************")
+colorful_print('info', "* Project: https://gitee.com/izmj/dump_helper          *")
+colorful_print('info', "* Current Version: v2.3                                *")
+colorful_print('info', "********************************************************")
+
+work_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+
 if sys.platform == "win32":
     exec_postfix = ".exe" 
+    exec_path = "third-party/x86_64/win32"
 elif sys.platform == "linux":
     exec_postfix = ""
+    exec_path = "third-party/x86_64/linux"
 elif sys.platform == "darwin":
     exec_postfix = ""
+    exec_path = "third-party/x86_64/darwin"
 else:
-    print(f"Unsupported platform: {sys.platform}")
+    colorful_print('error', f"Unsupported platform: {sys.platform}")
+    input("Press Enter to exit...")
     exit(1)
+
+# If current script is not end with .py
+if not sys.argv[0].endswith(".py"):
+    exec_path = 'res';
 
 def get_resource_path(relative_path):
     if getattr(sys, 'frozen', False):
@@ -22,30 +48,23 @@ def get_resource_path(relative_path):
         base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
 
-work_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
-
-print("********************************************************")
-print("* Project: https://gitee.com/izmj/dump_helper          *")
-print("* Current Version: v2.3                                *")
-print("********************************************************")
-
 # Check dump_syms
-dump_syms_path = get_resource_path(os.path.join("res", f"dump_syms{exec_postfix}"))
+dump_syms_path = get_resource_path(os.path.join(exec_path, f"dump_syms{exec_postfix}"))
 if not os.path.exists(dump_syms_path):
-    print(f"\n>>>> ERROR: Tools [{dump_syms_path}] not exists!\n")
+    colorful_print('error', f">>>> ERROR: Tools [{dump_syms_path}] not exists!")
     input("Press Enter to exit...")
     exit(1)
 
 # Check stackwalk
-stackwalk_path = get_resource_path(os.path.join("res" , f"minidump-stackwalk{exec_postfix}"))
+stackwalk_path = get_resource_path(os.path.join(exec_path , f"minidump-stackwalk{exec_postfix}"))
 if not os.path.exists(stackwalk_path):
-    print(f"\n>>>> ERROR: Tools [{stackwalk_path}] not exists!\n")
+    colorful_print('error', f">>>> ERROR: Tools [{stackwalk_path}] not exists!")
     input("Press Enter to exit...")
     exit(1)
 
 def dump_syms(so_path):
     if not os.path.exists(so_path):
-        print("\n>>>> ERROR: File [{so_path}] not exists!\n")
+        colorful_print('error', f">>>> ERROR: File [{so_path}] not exists!")
         return
 
     # dump symbols
@@ -55,19 +74,18 @@ def dump_syms(so_path):
         os.makedirs(so_target_path)
 
     dump_syms_cmd = f"{dump_syms_path} {so_path} > {so_target_path}.sym"
-    print(dump_syms_cmd)
+    
     subprocess.run(dump_syms_cmd, shell=True)
 
     # read id of symbols
     with open(f"{so_target_path}.sym", "r") as f:
         lines = f.readlines()
         if len(lines) < 2:
-            print("dump symbols failed")
+            colorful_print('error', "dump symbols failed")
             return
         id = lines[0].split()[3]
-        print("\n============= Symbol ID =============")
-        print(f"symbol id: {id}")
-        print("=====================================")
+        colorful_print('info', "********************** Symbol ID ***********************")
+        colorful_print('info', f"symbol id: {id}\n")
 
     # move symbols to symbols folder
     symbols_folder = os.path.join(work_dir, "symbols", os.path.basename(so_path), id)
@@ -76,34 +94,31 @@ def dump_syms(so_path):
 
     target_sym_path = os.path.join(symbols_folder, os.path.basename(so_path) + ".sym")
     if os.path.exists(target_sym_path):
-        print("symbols already exists. skip.")
+        colorful_print('warning', "symbols already exists! skip.")
         return
 
-    print(f"move {so_target_path}.sym to {target_sym_path}")
     os.rename(f"{so_target_path}.sym", target_sym_path)
 
 def stack_walk(dump_path):
     if not os.path.exists(dump_path):
-        print("dump file not exists: %s" % dump_path)
+        colorful_print('error', "dump file not exists: %s" % dump_path)
         return
 
     # stack walk
     symbol_path = os.path.join(work_dir, "symbols");
     stack_walk_cmd = f"{stackwalk_path} {dump_path} {symbol_path} > {dump_path}.stack"
-    print("Generate stack info for human...")
     subprocess.run(stack_walk_cmd, shell=True)
 
     stack_walk_cmd = f"{stackwalk_path} {dump_path} {symbol_path} --dump > {dump_path}.raw"
-    print("Generate stack info for machine...")
     subprocess.run(stack_walk_cmd, shell=True)
 
-    print("\n============= Recent Stack =============")
+    colorful_print('debug', "\n********************* Recent Stack *********************")
 
     # read stack
     with open(f"{dump_path}.stack", "r") as f:
         lines = f.readlines()
         if len(lines) < 2:
-            print("stack walk failed")
+            colorful_print('error', "stack walk failed!")
             return
         
         print_lines = 20
@@ -117,23 +132,36 @@ def stack_walk(dump_path):
                 if print_lines == 0:
                     break
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: %s <so_path>" % sys.argv[0])
-        print("Usage: %s <dump_path>" % sys.argv[0])
-        print("Usage: %s <so_path> <dump_path>" % sys.argv[0])
+    colorful_print('debug', "********************************************************")
 
-        input("Press Enter to exit...")
+if __name__ == "__main__":
+
+    args = []
+    if len(sys.argv) < 2:
+        # Wait user input library or dump file to process
+        while True:
+            file = input("Please input library or dump file(one by one):")
+            if file == "":
+                break
+
+            args.append(file)
+    else:
+        args = sys.argv[1:]
+
+    if len(args) == 0:
+        colorful_print("error", "No libraries or dump files input. Exit.")
         exit(1)
 
     # Parse libraries
-    for so_path in sys.argv[1:]:
+    for so_path in args:
         if so_path.endswith(".so") or so_path.endswith(".dll") or so_path.endswith(".lib"):
+            colorful_print("info", f"Processing library {so_path}...")
             dump_syms(so_path)
 
     # Parse dump files
-    for dump_path in sys.argv[1:]:
+    for dump_path in args:
         if dump_path.endswith(".dmp") or dump_path.endswith(".minidump"):
+            colorful_print("info", f"Processing dump file {dump_path}...")
             stack_walk(dump_path)
 
     # pause
